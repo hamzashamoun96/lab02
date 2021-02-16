f780a6451089da8e4b7609b717219dd0df2f3bf9
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-02-16 23:24:24 +0200

    lab3

--- js/app.js
+++ js/app.js
@@ -3,47 +3,17 @@
 
 let optionArr = [];
 let objectArr = [];
+let trigger = true;
 
 $.ajax('./data/page-1.json')
     .then(data => {
         data.forEach(value => {
             let newImage = new Images(value);
+            newImage.render();
             newImage.RenderOption();
-            newImage.RenderTemplate();
             objectArr.push(newImage);
         });
-        $('.photo-template').first().remove();
     })
-
-$('select').on('change', function () {
-    $('.photo-template').not(':first').remove();
-    objectArr.forEach((value) => {
-        if (value.keyword === $(this).val()) {
-            let newTemplate = $('.photo-template').first().clone();
-            newTemplate.find('h2').text(value.title);
-            newTemplate.find('img').attr('src', value.url);
-            newTemplate.find('p').text(value.description);
-            $('main').append(newTemplate);
-        }
-    })
-    
-
-    if ($(this).val() === 'default') {
-        objectArr.forEach((value) => {
-            let newTemplate = $('.photo-template').first().clone();
-            newTemplate.find('h2').text(value.title);
-            newTemplate.find('img').attr('src', value.url);
-            newTemplate.find('p').text(value.description);
-            $('main').append(newTemplate);
-
-        })
-    }
-    $('.photo-template').first().remove();
-})
-
-
-
-
 
 function Images(value) {
     this.url = value.image_url;
@@ -55,19 +25,101 @@ function Images(value) {
 Images.prototype.RenderOption = function () {
 
     if (!(optionArr.includes(this.keyword))) {
-
         let newOption = $('option').first().clone();
         newOption.text(this.keyword);
         newOption.val(this.keyword)
-        $('select').append(newOption);
+        $('#selectType').append(newOption);
         optionArr.push(this.keyword);
     }
 }
-Images.prototype.RenderTemplate = function () {
-
-    let newTemplate = $('.photo-template').first().clone();
-    newTemplate.find('h2').text(this.title);
-    newTemplate.find('img').attr('src', this.url);
-    newTemplate.find('p').text(this.description);
-    $('main').append(newTemplate);
+Images.prototype.render = function () {
+    let template = $('#template-section1').html();
+    let newImage = Mustache.render(template, this);
+    $('#container').append(newImage);
+    return newImage;
 }
+Images.prototype.render2 = function () {
+    let template = $('#template-section2').html();
+    let newImage = Mustache.render(template, this);
+    $('#container').append(newImage);
+    return newImage;
+}
+
+
+$('#btn1').on('click', function () {
+    $('.photo-template2').hide();
+    $('.photo-template1').show();
+})
+
+$('#btn2').on('click', function () {
+    $('.photo-template1').hide();
+    $('.photo-template2').show();
+    if (trigger) {
+        $.ajax('./data/page-2.json')
+            .then(data => {
+                data.forEach(value => {
+                    let newImage = new Images(value);
+                    newImage.render2();
+                    newImage.RenderOption();
+                    objectArr.push(newImage);
+                });
+            })
+    }
+    trigger = false;
+
+})
+
+$('#selectType').on('change', function () {
+    $('.photo-template1').hide();
+    $('.photo-template2').hide();
+
+    objectArr.forEach((value) => {
+        if (value.keyword === $(this).val()) {
+            $(`.${value.keyword}`).show();
+        }
+    })
+
+    if ($(this).val() === 'default') {
+        $('.photo-template1').show();
+        $('.photo-template2').hide();
+    }
+
+})
+
+$('#selectSort').on('change', function () {
+    $('.photo-template1').remove();
+    $('.photo-template2').remove();
+
+    if ($(this).val() === 'Horn') {
+        objectArr.sort((a, b) => {
+            if (a.horns < b.horns) {
+                return -1;
+            } else if (a.horns > b.horns) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        objectArr.forEach(value => {
+            value.render();
+        })
+    } else {
+        objectArr.sort((a, b) => {
+            if (a.title.toUpperCase() < b.title.toUpperCase()) {
+                //  console.log(a.name.toUpperCase())
+                return -1;
+            }
+            else if (a.title.toUpperCase() > b.title.toUpperCase()) {
+                return 1;
+            }
+
+            else {
+                return 0;
+            }
+        })
+        objectArr.forEach(value => {
+            value.render();
+        })
+    }
+
+})
